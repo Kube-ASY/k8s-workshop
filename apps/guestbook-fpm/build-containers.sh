4f1c7412fb5f0d0b0f18d16ac11ea4a9ee9ddd94
tbomberg <thorsten.bomberg@bertelsmann.de>
2021-03-10 12:58:02 +0100

    try to fix guestbook application

--- apps/guestbook-fpm/build-containers.sh
+++ apps/guestbook-fpm/build-containers.sh
@@ -4,9 +4,9 @@ export HARBOR=harbor2.csvcdev.vpc.arvato-systems.de/k8s-workshop
 
 # Build Guestbook
 
-docker build -t ${HARBOR}/gb-phpfpm:v6 docker/
+docker build -t ${HARBOR}/gb-phpfpm:v8 docker/
 
-docker push  ${HARBOR}/gb-phpfpm:v6
+docker push  ${HARBOR}/gb-phpfpm:v8
 
 docker build -t ${HARBOR}/gb-phpfpm:v7 docker-sts/
 
@@ -16,9 +16,9 @@ docker push  ${HARBOR}/gb-phpfpm:v7
 # Copy nginx container to Harbor
 
 #docker pull bitnami/nginx:1.19
-docker build -t ${HARBOR}/gb-nginx:latest -t ${HARBOR}/gb-nginx:1.19 -f docker/Dockerfile.nginx docker/
+docker build -t ${HARBOR}/gb-nginx:latest -t ${HARBOR}/gb-nginx:1.19.1 -f docker/Dockerfile.nginx docker/
 
-docker push ${HARBOR}/gb-nginx:1.19
+docker push ${HARBOR}/gb-nginx:1.19.1
 docker push ${HARBOR}/gb-nginx:latest
 
 # Copy redis Container to Harbor
