2b91a66d9a9a2a7d5b863959ae243b08fa563cd7
tbomberg <thorsten.bomberg@bertelsmann.de>
2021-10-21 09:26:33 +0200

    fix missing namespace

--- 05-KubernetesObjekte2/05-i-private/create-registry-secret.sh
+++ 05-KubernetesObjekte2/05-i-private/create-registry-secret.sh
@@ -4,7 +4,7 @@ echo "Enter Registry Token: "
 
 read RPWD
 
-kubectl create secret docker-registry myregistrykey \
+kubectl -n k8s-workshop create secret docker-registry myregistrykey \
   --docker-server=harbor.csvcdev.vpc.arvato-systems.de \
   --docker-username='robot$k8s-workshop-private+private' \
   --docker-password=$RPWD
